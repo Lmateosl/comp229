3d38dfd8c382767c8167d61365b1a17dce932db7
Lmateosl <mateosan67@hotmail..com>
2024-11-28 15:34:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo desde Express!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
+});
